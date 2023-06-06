cc525c6ed384adaf35b1bc24d1cbc24af925f4dd
db0 <mail@dbzer0.com>
2023-06-06 14:54:33 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# clipfree
+**WARNING: DO NOT CONSUME THIS LIBRARY**
+
+This library is a result of a [hlky sending a DMCA for 8 AGPL3 files](https://docs.github.com/articles/dmca-takedown-policy) missing an unnecessary attribution (see AGPL3 Section 7b), instead of asking informally like a reasonable person.
+
+As we cannot ensure that hlky won't abuse DMCA provisos again to fuel the vendeta they have against the AI Horde and violate the spirit of Free/Open Source software when it suits them, we cannot in good conscience suggest anyone else use this library.
+
+This library has been created to externalize the files in question until we can replace them
+
+Likewise, this library is rought extricated and only meant to be used in the context of hordelib
---
+++ clipfree/clip/text.py
@@ -0,0 +1,94 @@
+"""
+Copyright 2022-2023 hlky.
+Copyright 2023 hlky
+Copyright 2023-2024 Haidra Developers
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+import hashlib
+from concurrent.futures import ThreadPoolExecutor
+
+import clip
+import numpy as np
+import torch
+from loguru import logger
+
+from hordelib.cache import Cache
+from hordelib.utils.cast import autocast_cuda
+
+
+class TextEmbed:
+    def __init__(self, model_info, cache: Cache):
+        """
+        :param model: Loaded model from ModelManager
+        :param cache: Cache object
+        """
+        self.model_info = model_info
+        self.cache = cache
+        self.executor = ThreadPoolExecutor(
+            max_workers=1024,
+            thread_name_prefix="SaveThread",
+        )
+
+    @autocast_cuda
+    def _batch(self, text_list: list):
+        for text in text_list:
+            if isinstance(text["prompt"], bytes):
+                text["prompt"] = text["prompt"].decode("utf-8")
+            text["hash"] = hashlib.sha256(text["prompt"]).hexdigest()
+        text_tokens = [
+            clip.tokenize(text["prompt"], truncate=True).to(self.model_info["device"]) for text in text_list
+        ]
+        text_tokens = torch.cat(text_tokens, dim=0)
+        with torch.no_grad():
+            text_features = self.model_info["model"].encode_text(text_tokens).float()
+        for text_embed_array, text in zip(text_features, text_list):
+            self.executor.submit(self._save, text_embed_array, text["hash"])
+            self.cache.add_sqlite_row(text["filename"], text["hash"], text["hash"])
+
+    def _save(self, text_embed_array, text_hash):
+        text_embed_array /= text_embed_array.norm(dim=-1, keepdim=True)
+        np.save(
+            f"{self.cache.cache_dir}/{text_hash}",
+            text_embed_array.float().cpu().detach().numpy(),
+        )
+
+    @autocast_cuda
+    def __call__(self, text: str, check_cache: bool = False, filename: str = None):
+        """
+        :param text: Text to embed
+        :param check_cache: Check cache for text
+        :param filename: Filename to save to cache
+        If text is not in cache, embed it and save it to cache
+        """
+        if isinstance(text, bytes):
+            text = text.decode("utf-8")
+        text_hash = hashlib.sha256(text.encode("utf-8")).hexdigest()
+        if check_cache:
+            cached = self.cache.get(file_hash=text_hash)
+            if cached:
+                return cached
+        text_tokens = clip.tokenize([text], truncate=True).to(self.model_info["device"])
+        with torch.no_grad():
+            text_features = self.model_info["model"].encode_text(text_tokens).float()
+        text_features /= text_features.norm(dim=-1, keepdim=True)
+        text_embed_array = text_features.cpu().detach().numpy()
+        if filename:
+            np.save(f"{self.cache.cache_dir}/{text_hash}", text_embed_array)
+            self.cache.add_sqlite_row(filename, text_hash, text_hash)
+            return None
+
+        np.save(f"{self.cache.cache_dir}/{text_hash}", text_embed_array)
+        self.cache.add_sqlite_row(text, text_hash, text_hash)
+        return None
